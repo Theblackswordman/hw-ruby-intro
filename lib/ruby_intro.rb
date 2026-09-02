d8be8d74c9176de5e848d0933de343e0cded00d3
Huy Hoang <hvghuy061107@gmail.com>
2026-09-01 23:21:02 -0700

    Finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,34 +3,45 @@
 # Part 1
 
 def sum(arr)
-  array.sum
+  arr.sum
 end
 
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  arr.max(2).sum
 end
 
 def sum_to_n?(arr, n)
-  # YOUR CODE HERE
+  arr.combination(2).any? { |a, b| a + b == n}
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant?(s)
-  # YOUR CODE HERE
+  s.match?(/\A[bcdfghjklmnpqrstvwyz]/i)
 end
 
 def binary_multiple_of_4?(s)
-  # YOUR CODE HERE
+  s.match?(/\A(0|[01]+00)\z/)
 end
 
 # Part 3
 
 class BookInStock
-  # YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty?
+    raise ArgumentError if price <= 0
+    @isbn = isbn
+    @price = Float(price)
+  end
+
+  def price_as_string
+    format("$%.2f", price)
+  end
 end
